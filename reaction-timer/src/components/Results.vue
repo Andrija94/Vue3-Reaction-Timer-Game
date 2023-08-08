c0d56f62abb732e488ca121afb18c7458d7dcb4a
Andrija94 <andrija.velickovic94@gmail.com>
2023-08-08 15:43:31 +0200

    add block and result components and functions to test reaction speed of a click

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Your result is: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Resuts',
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail pace...'
+        }
+    }
+
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1,4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
